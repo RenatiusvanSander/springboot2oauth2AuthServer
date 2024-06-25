f2669bfaab8b04e4699bfa71a7c9a7e452dac5c8
RenatiusvanSander <remad@web.de>
2024-06-25 22:38:19 +0200

    master: initial setup of spring boot 3 app

---
+++ src/test/java/edu/remad/springboot3oauth2authserver/Springboot3oauth2authserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.remad.springboot3oauth2authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3oauth2authserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
