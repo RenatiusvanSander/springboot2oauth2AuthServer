e707b954e32dc33b8c40034e4da576189b493c91
RenatiusvanSander <remad@web.de>
2024-06-25 22:38:19 +0200

    master: initial setup of spring boot 3 app with MySQL 8 data source

---
+++ src/main/java/edu/remad/springboot3oauth2authserver/Springboot3oauth2authserverApplication.java
@@ -0,0 +1,12 @@
+package edu.remad.springboot3oauth2authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3oauth2authserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3oauth2authserverApplication.class, args);
+	}
+}
